183ca344e4670ec0380c695828189f09a8a86a76
Darshan-Raval303107 <ravaldarshan079@gmail.com>
2026-07-25 14:21:33 +0530

    LC-maximum-product-of-two-digits [Runtime: 0 ms; Memory: 9 MB]

--- sorting/maximum-product-of-two-digits.cpp
+++ sorting/maximum-product-of-two-digits.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int maxProduct(int n) {
-        int maximum = 0;
         vector<int>v;
-        int k = n;
 
-        while(k > 0) {
-            v.push_back(k%10);
-            k /= 10;
+        while(n > 0) {
+            v.push_back(n%10);
+            n /= 10;
         } 
 
-        for(int i = 0;i < v.size();i++) {
-            for(int j = i  + 1;j < v.size();j++) {
-                int ans = v[i] * v[j];
-                maximum = max(maximum,ans);
-            }
-        }  
+        sort(v.begin(),v.end());
 
-        return maximum;
+
+        return (v[v.size() - 1]*v[v.size() - 2]);
     }
 };
