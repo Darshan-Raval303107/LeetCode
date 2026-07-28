18f06ec1ac00441d5263e8fe227c7be57f79e8eb
Darshan-Raval303107 <ravaldarshan079@gmail.com>
2026-07-28 15:31:34 +0530

    LC-move-zeroes [Runtime: N/A; Memory: N/A]

--- array/move-zeroes.cpp
+++ array/move-zeroes.cpp
@@ -7,7 +7,7 @@ public:
         }
         int j = 0;
         for (int i = 0; i < n; i++) {
-            if (nums[j] == 0 && nums[i] != 0) {
+            if (nums[j] == 0) {
                 swap(nums[i], nums[j]);
             }
             if (nums[j] != 0) {
