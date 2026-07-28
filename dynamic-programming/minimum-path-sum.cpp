acd01a9e98172d4d59417af4e6a7f845fc8c7da0
Darshan-Raval303107 <ravaldarshan079@gmail.com>
2026-07-28 14:54:44 +0530

    LC-minimum-path-sum [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/minimum-path-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int solve(vector<vector<int>> grid, int i, int j, int m, int n, int val) {
+        if (i >= m || j >= n)
+            return INT_MAX;
+        val += grid[i][j];
+        if (i == m - 1 && j == n - 1) {
+            return val;
+        }
+
+        int right = solve(grid, i + 1, j, m, n, val);
+        int down = solve(grid, i, j + 1, m, n, val);
+
+        return min(right, down);
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        int i = 0, j = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+
+        return solve(grid, i, j, m, n, 0);
+    }
+};
