28a9555d87a39d306c45fe400aa67c6ae848b5e0
Darshan-Raval303107 <ravaldarshan079@gmail.com>
2026-08-08 14:21:40 +0530

    LC-monotonic-array [Runtime: N/A; Memory: N/A]

--- array/monotonic-array.cpp
+++ array/monotonic-array.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
-        int n = nums.size();
-        bool monotone = true;
-        int i = 0,j = n - 1;
-
-        if(nums[i] < nums[j]) {
-            j = 1;
-            for(i = 0;i < n;i++) {
-                if(j == n) break;
-                if(nums[i] > nums[j]) {
-                    monotone = false;
-                    break;
-                }
-                j++;
-            }
-        }else{
-            j = 1;
-            for(i = 0;i < n;i++) {
-                if(j == n) break;
-                if(nums[i] < nums[j]) {
-                    monotone = false;
-                    break;
-                }
-                j++;
+        int n=nums.size();
+        int count1=0,count2=0;
+        
+        for(int i=1;i<n;i++){
+            if(nums[i]>=nums[i-1]){
+                count1++;
             }
         }
-
-        
-
-        return monotone;
+        if(count1 == n - 1) return true;
+        for(int i=1;i<n;i++){
+            if(nums[i]<=nums[i-1]){
+                count2++;
+            }
+        }
+        if(count2==n-1){
+            return true;
+        }
+        return false;
     }
 };
