e9142107954bb1132aaaf00eac299e0a10d9776e
Darshan-Raval303107 <ravaldarshan079@gmail.com>
2026-08-09 09:59:28 +0530

    LC-merge-sorted-array [Runtime: N/A; Memory: N/A]

--- array/merge-sorted-array.cpp
+++ array/merge-sorted-array.cpp
@@ -3,26 +3,12 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         vector<int> result;
 
-        if (m > 0) {
-            int k = nums1[0];
-            result.push_back(nums1[0]);
-
-            for (int i = 1; i < m; i++) {
-                if (k == nums1[i]) continue;
-                result.push_back(nums1[i]);
-                k = nums1[i];
-            }
+        for (int i = 0; i < m; i++) {
+            result.push_back(nums1[i]);
         }
 
-        if (n > 0) {
-            int k = nums2[0];
-            result.push_back(nums2[0]);
-
-            for (int i = 1; i < n; i++) {
-                if (k == nums2[i]) continue;
-                result.push_back(nums2[i]);  
-                k = nums2[i];
-            }
+        for (int i = 0; i < n; i++) {
+            result.push_back(nums2[i]);
         }
 
         sort(result.begin(), result.end());
