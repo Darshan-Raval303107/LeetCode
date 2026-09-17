98f08bc25557aa62122aa3f47a5c69ff8387289f
Darshan-Raval303107 <ravaldarshan079@gmail.com>
2026-09-17 21:37:26 +0530

    LC-next-permutation [Runtime: N/A; Memory: N/A]

--- array/next-permutation.cpp
+++ array/next-permutation.cpp
@@ -1,21 +1,6 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int cnt = -1;
-        for(int i = nums.size() - 2;i >= 0;i--) {
-            if(nums[i] < nums[i + 1]) {
-                cnt = i;
-                break;
-            }
-        }
-        if(cnt != -1) {
-            for(int i = nums.size() - 1; i > cnt; i--) {
-                if(nums[i] > nums[cnt]) {
-                    swap(nums[i], nums[cnt]);
-                    break;
-                }
-            }
-        }
-        reverse(nums.begin() + cnt + 1,nums.end());
+        next_permutation(nums.begin(), nums.end());
     }
 };
